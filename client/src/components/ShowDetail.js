8f0d4c1fdc280cdab16377da103457d996ace274
bmolina-nyc <bruce.molina.81@gmail.com>
2018-06-12 21:46:44 -0400

    quick button add for my individual movie component

--- client/src/components/ShowDetail.js
+++ client/src/components/ShowDetail.js
@@ -15,25 +15,25 @@ class ShowDetail extends React.Component{
         release_date: ""
     }
     
-this.componentWillMount = () =>{
-  const movieId = this.props.match.params.movieId
+    this.componentWillMount = () =>{
+    const movieId = this.props.match.params.movieId
 
-  movieDetail(movieId).then((result)=>{
-    result.genres.map((el) =>{return this.state.genre.push(el.name)})    
-    this.setState({
-        title: result.original_title, 
-        overview: result.overview, 
-        tagline: result.tagline,
-        poster: result.poster_path,
-        release_date: result.release_date,
-    })
-    console.log(this.state)
-  })
+    movieDetail(movieId).then((result)=>{
+        result.genres.map((el) =>{return this.state.genre.push(el.name)})    
+        this.setState({
+            title: result.original_title, 
+            overview: result.overview, 
+            tagline: result.tagline,
+            poster: result.poster_path,
+            release_date: result.release_date,
+        })
+        console.log(this.state)
+        })
+      }
+    }
 
-}
-
-    
-
+    onClick = () => {
+        this.props.history.push('/search')
     }
 
     render(){
@@ -45,12 +45,14 @@ this.componentWillMount = () =>{
               <h4>{this.state.genre.join(", ")}</h4>
               <h4>Release Date: {this.state.release_date}</h4>
               <h5>Synopsis: {this.state.overview}</h5>
+
+              <button onClick={this.onClick}>Back to Results</button>
             </div>
         )
     }
 }
 
-// genre: result.genres.first.name, 
+
 
 export default ShowDetail;
 
